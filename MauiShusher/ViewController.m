31025609eb175fd0396bcccfc11f368b66046fe2
Aaron Schachter <aaronschachter@gmail.com>
2017-04-30 11:31:07 -0700

    Adds skeleton UI, gitignore

--- MauiShusher/ViewController.m
+++ MauiShusher/ViewController.m
@@ -1,29 +1,89 @@
 //
 //  ViewController.m
-//  MauiShusher
+//  SleepZone
 //
-//  Created by Aaron Schachter on 4/29/17.
+//  Created by Aaron Schachter on 4/14/17.
 //  Copyright © 2017 New School Old School. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-
-@end
+    
+    @property (assign, nonatomic) BOOL isPlayingRain;
+    @property (assign, nonatomic) BOOL isPlayingShusher;
+    @property (strong, nonatomic) AVAudioPlayer *rainPlayer;
+    @property (strong, nonatomic) AVAudioPlayer *shusherPlayer;
+    @property (weak, nonatomic) IBOutlet UISwitch *shusherSwitch;
+    @property (weak, nonatomic) IBOutlet UIView *shusherContainerView;
+    @property (weak, nonatomic) IBOutlet UIView *rainContainerView;
+    
+    @property (weak, nonatomic) IBOutlet UISwitch *rainSwitch;
+  
+    @end
 
 @implementation ViewController
-
+    
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    [[AVAudioSession sharedInstance] setActive: YES error: nil];
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    
+    NSString *shusherSoundFilePath = [NSString stringWithFormat:@"%@/shh.m4a",[[NSBundle mainBundle] resourcePath]];
+    NSURL *shusherSoundFileURL = [NSURL fileURLWithPath:shusherSoundFilePath];
+    
+    self.shusherPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:shusherSoundFileURL error:nil];
+    self.shusherPlayer.numberOfLoops = -1;
+    
+    [self.shusherSwitch setOn:NO];
+    self.isPlayingShusher = NO;
+    
+    NSString *rainFilePath = [NSString stringWithFormat:@"%@/rain.mp3",[[NSBundle mainBundle] resourcePath]];
+    NSURL *rainFileURL = [NSURL fileURLWithPath:rainFilePath];
+    self.rainPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:rainFileURL error:nil];
+    self.rainPlayer.numberOfLoops = -1;
+    [self.rainPlayer setVolume:0.75];
+    
+    [self.rainSwitch setOn:NO];
+    self.isPlayingRain = NO;
+    
+    UITapGestureRecognizer *shusherTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleShusherTap:)];
+    [self.shusherContainerView addGestureRecognizer:shusherTap];
+    UITapGestureRecognizer *rainTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleRainTap:)];
+    [self.rainContainerView addGestureRecognizer:rainTap];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+- (void)handleRainTap:(UITapGestureRecognizer *)recognizer {
+    [self togglePlayingRain];
 }
-
-
-@end
+    
+- (void)handleShusherTap:(UITapGestureRecognizer *)recognizer {
+    [self togglePlayingShusher];
+}
+    
+- (void)togglePlayingRain {
+    self.isPlayingRain = !self.isPlayingRain;
+    
+    if (self.isPlayingRain) {
+        [self.rainPlayer play];
+    } else {
+        [self.rainPlayer pause];
+    }
+    [self.rainSwitch setOn:self.isPlayingRain animated:YES];
+}
+    
+- (void)togglePlayingShusher {
+    self.isPlayingShusher = !self.isPlayingShusher;
+    
+    if (self.isPlayingShusher) {
+        [self.shusherPlayer play];
+    } else {
+        [self.shusherPlayer pause];
+    }
+    [self.shusherSwitch setOn:self.isPlayingShusher animated:YES];
+}
+    
+    @end
